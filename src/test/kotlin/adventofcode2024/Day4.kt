b8d4cd4150b1b0a040cfc6e1528f4139571afbf2
Michel Schudel <michel.schudel@gmail.com>
2024-12-04 15:03:23 +0100

    Improvements

--- src/test/kotlin/adventofcode2024/Day4.kt
+++ src/test/kotlin/adventofcode2024/Day4.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test
 import kotlin.math.abs
 
 class Day4 {
-
     data class Coordinate(
         val x: Int,
         val y: Int,
@@ -42,47 +41,69 @@ class Day4 {
         return coordinates.sumOf { countWords(it, word, 0, coordinates) }
     }
 
-    private fun countWords(coordinate: Coordinate,
-                           word: String,
-                           index: Int,
-                           coordinates: List<Coordinate>,
-                           xDirection: Int? = null,
-                           yDirection: Int? = null   ): Int {
-        if (coordinate.c != word[index]) {
-            return 0
+    private fun countWords(
+        coordinate: Coordinate,
+        word: String,
+        index: Int,
+        coordinates: List<Coordinate>,
+        xDirection: Int? = null,
+        yDirection: Int? = null,
+    ): Int {
+        if (coordinate.c != word[index]) return 0
+        if (coordinate.c == word[index] && index == word.length - 1) return 1
+        val surroundingCoordinates =
+            coordinates.filter {
+                abs(it.x - coordinate.x) <= 1 &&
+                    abs(it.y - coordinate.y) <= 1 &&
+                    it != coordinate &&
+                    (xDirection == null || it.x - coordinate.x - xDirection == 0) &&
+                    (yDirection == null || it.y - coordinate.y - yDirection == 0)
+            }
+        return surroundingCoordinates.sumOf {
+            countWords(
+                coordinate = it,
+                word = word,
+                index = index + 1,
+                coordinates = coordinates,
+                xDirection = it.x - coordinate.x,
+                yDirection = it.y - coordinate.y,
+            )
         }
-        if (coordinate.c == word[index] && index == word.length - 1) {
-            return 1
-        }
-        val surroundingCoordinates = coordinates.filter {
-            abs(it.x - coordinate.x) <= 1 && abs(it.y - coordinate.y) <= 1 && it != coordinate
-                    && (xDirection == null || it.x - coordinate.x - xDirection == 0)
-                    && (yDirection == null || it.y - coordinate.y - yDirection == 0)
-        }
-        return surroundingCoordinates.sumOf { countWords(it, word, index + 1, coordinates, it.x - coordinate.x, it.y - coordinate.y) }
     }
 
-    private fun solvePart2(coordinates: List<Coordinate>): Int {
-        val allStartingCoordinates = coordinates.filter { it.c == 'A' }
-        return allStartingCoordinates.count { isCross(it, coordinates) }
-    }
+    private fun solvePart2(coordinates: List<Coordinate>): Int = coordinates.filter { it.c == 'A' }.count { isXMas(it, coordinates) }
 
-    private fun isCross(coordinate: Coordinate, coordinates: List<Coordinate>): Boolean {
-        val topLeft = coordinates.find { it.x == coordinate.x -1 && it.y  == coordinate.y -1 }
-        val topRight = coordinates.find { it.x == coordinate.x +1 && it.y  == coordinate.y -1 }
-        val bottomLeft = coordinates.find { it.x == coordinate.x -1 && it.y  == coordinate.y +1 }
-        val bottomRight = coordinates.find { it.x == coordinate.x+1 && it.y  == coordinate.y +1 }
-        if (topRight == null || topLeft == null || bottomLeft == null || bottomRight == null) return false
-        if (topLeft.c == 'M' && topRight.c == 'M' && bottomLeft.c == 'S' && bottomRight.c == 'S') return true
-        if (topLeft.c == 'S' && topRight.c == 'S' && bottomLeft.c == 'M' && bottomRight.c == 'M') return true
-        if (topLeft.c == 'M' && topRight.c == 'S' && bottomLeft.c == 'M' && bottomRight.c == 'S') return true
-        if (topLeft.c == 'S' && topRight.c == 'M' && bottomLeft.c == 'S' && bottomRight.c == 'M') return true
-        return false
+    private fun isXMas(
+        coordinate: Coordinate,
+        coordinates: List<Coordinate>,
+    ): Boolean {
+        val s =
+            coordinates
+                .filter {
+                    abs(it.x - coordinate.x) == 1 && abs(it.y - coordinate.y) == 1 && (it.c == 'M' || it.c == 'S')
+                }.map { it.c }
+                .joinToString("")
+        return when (s.length) {
+            4 -> s == "MMSS" || s == "SSMM" || s == "MSMS" || s == "SMSM"
+            else -> false
+        }
     }
 
     // model builders
     private fun buildModelFromInput(fileName: String): List<Coordinate> {
         val lines = loadStringLines(fileName)
-        return lines.mapIndexed { y, line -> line.chars().toArray().mapIndexed { x, c -> Coordinate(x = x, y = y, c = c.toChar()) } }.flatten().sortedWith(compareBy({ it.y }, { it.x }))
+        return lines
+            .mapIndexed {
+                    y,
+                    line,
+                ->
+                line.chars().toArray().mapIndexed {
+                        x,
+                        c,
+                    ->
+                    Coordinate(x = x, y = y, c = c.toChar())
+                }
+            }.flatten()
+            .sortedWith(compareBy({ it.y }, { it.x }))
     }
 }
