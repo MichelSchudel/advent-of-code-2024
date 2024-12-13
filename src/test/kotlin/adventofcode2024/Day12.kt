475afd721f1cfdea329d0f7c204296611a2f6cfd
Michel Schudel <michel.schudel@gmail.com>
2024-12-13 11:33:12 +0100

    Day 12 part 2

--- src/test/kotlin/adventofcode2024/Day12.kt
+++ src/test/kotlin/adventofcode2024/Day12.kt
@@ -13,45 +13,137 @@ class Day12 {
     @Test
     fun testPart1Trivial() {
         val model = buildModelFromInput("/test-input-day-12-trivial.txt")
-        assertThat(solvePart1(model)).isEqualTo(140)
+        assertThat(solve(model, discountApplied = false)).isEqualTo(140)
     }
 
     @Test
     fun testPart1Trivial2() {
         val model = buildModelFromInput("/test-input-day-12-trivial2.txt")
-        assertThat(solvePart1(model)).isEqualTo(772)
+        assertThat(solve(model, discountApplied = false)).isEqualTo(772)
     }
 
     @Test
     fun testPart1() {
         val model = buildModelFromInput("/test-input-day-12.txt")
-        assertThat(solvePart1(model)).isEqualTo(1930)
+        assertThat(solve(model, discountApplied = false)).isEqualTo(1930)
     }
 
     @Test
-    fun solvePart1() {
+    fun solve() {
         val model = buildModelFromInput("/real-input-day-12.txt")
-        println(solvePart1(model))
+        println(solve(model, discountApplied = false))
     }
 
-    private fun solvePart1(model: List<Coordinate>): Int {
+    @Test
+    fun testPart2() {
+        val model = buildModelFromInput("/test-input-day-12-trivial.txt")
+        assertThat(solve(model, discountApplied = true)).isEqualTo(80)
+    }
+
+    @Test
+    fun solvePart2() {
+        val model = buildModelFromInput("/real-input-day-12.txt")
+        println(solve(model, discountApplied = true))
+    }
+
+    private fun solve(
+        model: List<Coordinate>,
+        discountApplied: Boolean,
+    ): Int {
         val visited = mutableListOf<Coordinate>()
         val groups =
             model.map { coordinate ->
-                println("Next coordinate")
                 getGroup(coordinate, null, model, visited, mutableListOf())
             }
         val actualGroups = groups.filter { it.size > 0 }
-        return actualGroups.map { calculatePrice(it, model) }.sum()
+        return actualGroups.map { calculatePrice(it, model, discountApplied) }.sum()
     }
 
     private fun calculatePrice(
         group: List<Coordinate>,
         model: List<Coordinate>,
+        discountApplied: Boolean,
     ): Int {
         val area = group.size
-        val perimeter = group.map { countSides(it, model) }.sum()
-        return area * perimeter
+        return if (discountApplied) {
+            val corners = getCorners(group)
+            area * corners
+        } else {
+            val perimeter = group.sumOf { countSides(it, model) }
+            area * perimeter
+        }
+    }
+
+    private fun getCorners(group: List<Coordinate>): Int =
+        group.sumOf {
+            getCorners(it, group)
+        }
+
+    private fun getCorners(
+        coordinate: Coordinate,
+        group: List<Coordinate>,
+    ): Int {
+        var corners = 0
+        // inner corners
+        // top right
+        if (group.any { it.x == coordinate.x + 1 && it.y == coordinate.y } &&
+            group.any { it.x == coordinate.x && it.y == coordinate.y - 1 } &&
+            group.none { it.x == coordinate.x + 1 && it.y == coordinate.y - 1 }
+        ) {
+            corners++
+        }
+        // bottom right
+        if (group.any { it.x == coordinate.x + 1 && it.y == coordinate.y } &&
+            group.any { it.x == coordinate.x && it.y == coordinate.y + 1 } &&
+            group.none { it.x == coordinate.x + 1 && it.y == coordinate.y + 1 }
+        ) {
+            corners++
+        }
+        // bottom left
+        if (group.any { it.x == coordinate.x - 1 && it.y == coordinate.y } &&
+            group.any { it.x == coordinate.x && it.y == coordinate.y + 1 } &&
+            group.none { it.x == coordinate.x - 1 && it.y == coordinate.y + 1 }
+        ) {
+            corners++
+        }
+        // top left
+        if (group.any { it.x == coordinate.x - 1 && it.y == coordinate.y } &&
+            group.any { it.x == coordinate.x && it.y == coordinate.y - 1 } &&
+            group.none { it.x == coordinate.x - 1 && it.y == coordinate.y - 1 }
+        ) {
+            corners++
+        }
+
+        // outer corners
+        //  top and right
+        if (group.none { it.x == coordinate.x + 1 && it.y == coordinate.y } &&
+            group.none { it.x == coordinate.x && it.y == coordinate.y - 1 }
+        ) {
+            corners++
+        }
+
+        // top and left
+        if (group.none { it.x == coordinate.x - 1 && it.y == coordinate.y } &&
+            group.none { it.x == coordinate.x && it.y == coordinate.y - 1 }
+        ) {
+            corners++
+        }
+
+        // bottom and left
+        if (group.none { it.x == coordinate.x - 1 && it.y == coordinate.y } &&
+            group.none { it.x == coordinate.x && it.y == coordinate.y + 1 }
+        ) {
+            corners++
+        }
+
+        // bottom and right
+        if (group.none { it.x == coordinate.x + 1 && it.y == coordinate.y } &&
+            group.none { it.x == coordinate.x && it.y == coordinate.y + 1 }
+        ) {
+            corners++
+        }
+
+        return corners
     }
 
     private fun countSides(
@@ -77,13 +169,10 @@ class Day12 {
         visited: MutableList<Coordinate>,
         group: MutableList<Coordinate>,
     ): List<Coordinate> {
-        println("visting coordinate $coordinate, current plantType $currentPlantType")
         if (visited.contains(coordinate)) {
-            println("coordinate $coordinate already visited or added to group")
             return group
         }
         if (currentPlantType == null || coordinate.plantType == currentPlantType) {
-            println("adding $coordinate to group")
             visited.add(coordinate)
             group.add(coordinate)
             model
